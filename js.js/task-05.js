702cd9ca5ece6902cbf97dd80c47f2db57650129
Alex Zhadanov <jadanov89@gmail.com>
2022-07-07 13:40:06 +0300

    add 5 task

--- js.js/task-05.js
+++ js.js/task-05.js
@@ -1,3 +1,16 @@
+const nameType = document.querySelector('#name-input');
+const nameText = document.querySelector('#name-output');
+
+nameType.addEventListener('input', function (event) {
+    nameText.textContent = event.currentTarget.value;
+
+    if (event.currentTarget.value === '') {
+        nameText.textContent = 'Anonymous';
+    }
+    return
+});
+
+/*
 let nameBase = 'Anonymous';
 const nameType = document.querySelector('#name-input');
 const nameText = document.querySelector('#name-output');
@@ -7,8 +20,11 @@ const nameTypeAdd = (event) => {
         nameText.textContent = nameBase;
     }
     else {
-    nameText.textContent = event.currentTarget.value
     }
 };
 
 nameType.addEventListener('change', nameTypeAdd);
+*/
+
+
+
